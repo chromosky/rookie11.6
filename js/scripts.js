3400a83725c85c44e5856f1650a66437779f6bf5
JanWrotkowski <trojmiasto2@wp.pl>
2017-04-13 12:08:23 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){
+
+    var url = 'https://restcountries.eu/rest/v1/name/';
+    var countriesList = $('#countries');
+
+    function searchCountries() {
+        var countryName = $('#country-name').val();
+        
+        if(!countryName.length) countryName = 'Poland';
+        
+        $.ajax({
+            url: url + countryName,
+            method: 'GET',
+            success: showCountriesList
+        });
+    }
+    
+    $('#search').click(searchCountries);
+
+    function showCountriesList(resp) {
+        countriesList.empty();
+        var i = 1;
+        resp.forEach(function(item){
+            $('<li>').text(i++ + ' ' + item.name).appendTo(countriesList);
+        });
+    }  
+    
+});
